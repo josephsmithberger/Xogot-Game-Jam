41ad530ca7aae35656ed00f198b5c617d8928731
josephsmithberger <josephsmithberger@gmail.com>
2025-06-20 19:33:55 -0400

    Web Singleton

--- button.gd
+++ button.gd
@@ -2,8 +2,5 @@ extends Button
 
 
 func _on_pressed() -> void:
-	# On some browsers (like Safari on iOS 13+), we need to explicitly
-	# request permission from the user to access motion data.
-	# This must be triggered by a user action, like a button press.
 	if OS.has_feature("web"):
 		JavaScriptBridge.eval("requestMotionPermission()")
--- gyro_test.gd
+++ gyro_test.gd
@@ -1,36 +1,13 @@
-extends MeshInstance3D
+extends Node3D
 
-var latest_beta: float = 0.0
-var latest_gamma: float = 0.0
-var latest_alpha: float = 0.0
-
-# This function will be called from JavaScript with gyroscope data.
-func update_rotation_from_js(beta: float, gamma: float, alpha: float) -> void:
-	# Store the latest values received from JavaScript.
-	latest_beta = beta
-	latest_gamma = gamma
-	latest_alpha = alpha
-
-# Called when the node enters the scene tree.
-func _ready() -> void:
-	# Check if the game is running in a web browser.
-	if OS.has_feature("web"):
-		# Create a Callable that points to our update_rotation_from_js function.
-		var callable = Callable(self, "update_rotation_from_js")
-		# Create a JavaScript callback object from our Godot Callable.
-		var js_callback = JavaScriptBridge.create_callback(callable)
-		# Call the 'initializeGyroListener' function in the HTML shell,
-		# passing our JavaScript callback object as an argument.
-		JavaScriptBridge.eval("initializeGyroListener(%s)" % js_callback)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	# The values from the browser are in degrees. Godot uses radians for rotation.
-	# DeviceOrientationEvent axes:
-	# beta: rotation around x-axis
-	# gamma: rotation around y-axis
-	# alpha: rotation around z-axis
-	# Note: You may need to swap or negate axes for your desired orientation.
-	rotation.x = deg_to_rad(latest_beta)
-	rotation.y = deg_to_rad(latest_gamma)
-	rotation.z = deg_to_rad(latest_alpha)
+func _process(delta):
+	# Get the gravity vector from the device
+	var gravity = Input.get_gravity()
+	# Calculate the rotation from the gravity vector
+	var up = -gravity.normalized()
+	var basis = Basis()
+	basis.y = up
+	basis.x = basis.y.cross(Vector3.FORWARD).normalized()
+	basis.z = basis.x.cross(basis.y).normalized()
+	# Set the node's rotation to match the device orientation
+	self.basis = basis
