b3152e8841f197b1a0b4154a954ad2937003314e
josephsmithberger <josephsmithberger@gmail.com>
2025-06-28 21:09:17 -0400

    pre leader board

--- game/main/ball_handle.gd
+++ game/main/ball_handle.gd
@@ -4,3 +4,13 @@ signal new_level
 
 func _next_level(level: int) -> void:
 	new_level.emit()
+
+
+
+func _on_finished_level_next_level() -> void:
+	get_child(0).queue_free()
+
+	var ball_scene := preload("res://game/main/ball.tscn")
+	var ball_instance := ball_scene.instantiate()
+	add_child(ball_instance)
+	print("did")
--- game/main/camera.gd
+++ game/main/camera.gd
@@ -3,5 +3,10 @@ extends Camera3D
 var follow_speed := 2
 
 func _physics_process(delta: float) -> void:
-	position.x = lerp(position.x, ball.position.x, follow_speed * delta)
-	position.z = lerp(position.z, ball.position.z, follow_speed * delta)
+	if ball:
+		position.x = lerp(position.x, ball.position.x, follow_speed * delta)
+		position.z = lerp(position.z, ball.position.z, follow_speed * delta)
+
+func _process(delta: float) -> void:
+	if not ball:
+		ball = $"../Node3D".get_child(0)
